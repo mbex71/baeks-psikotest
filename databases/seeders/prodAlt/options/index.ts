66ffed6429cf1d1faf374ced56cddba48e900380
Muhammad Firdaus <mbex71@gmail.com>
2022-02-11 19:38:03 +0700

    fixing mekanisme ujian

---
+++ databases/seeders/prodAlt/options/index.ts
@@ -0,0 +1,11 @@
+import angka from "./angka";
+import huruf from './huruf'
+import symbol from "./symbol";
+
+const optoins = [ 
+    ...angka, 
+    ...huruf ,
+    ...symbol
+]
+
+export default optoins
